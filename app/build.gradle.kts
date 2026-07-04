f4560c219ff3ba6f0f25b19fa643ceb8b228f83c
Zenlixir <zenlixirz@gmail.com>
2026-07-04 15:41:46 +0800

    Raise minSdk to 31 (Android 12+)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rynekryz.dormguard"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
